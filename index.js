89b311f9b77d363ad3fbe0ecfae5dbf6c3ed88f6
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-19 18:22:05 +0100

    create scrolling function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
     const rootElement=document.querySelector('#root');
     const sections=document.querySelectorAll('section');
+    let currentSectionIndex=0;
     document.addEventListener('mousewheel', function(event){
+        const direction =event.wheelDelta < 0 ? 1 : -1;
+        if(direction===1){
+            let isLastSection=currentSectionIndex===sections.length-1;
+            if(isLastSection) return;
+
+        }else if(direction===-1){
+            const firsSection=currentSectionIndex===0;
+            if(firsSection) return;
+        }
+        currentSectionIndex=currentSectionIndex+direction;
         
     })
 })
